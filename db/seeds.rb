0916ff9eb80de557afd3e1a96522d301d1b5e301
wacawa <mikkunmitsuaki@gmail.com>
2020-02-11 06:43:50 +0000

    Finish top-page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,12 @@
 # coding: utf-8
 
-User.create!( name: "Sample User",
+User.create!( name: "管理者",
               email: "sample@email.com",
               password: "password",
               password_confirmation: "password")
+              
+User.create!( name: "あき",
+              email: "sample-1@email.com",
+              password: "password",
+              password_confirmation: "password")
+                            
